d4f3545700c834d23de742f2b2892951b48855ce
mort1skoda <mort1skoda@gmail.com>
2023-01-30 05:32:17 +0100

    [2023-01-30 05:23:31]

--- caps-to-esc.sh
+++ caps-to-esc.sh
@@ -3,9 +3,8 @@
 echo "Hello from test.sh"
 sleep 5
 echo "That was 5 sec. pause!"
-setxkbmap -option "caps:swapescape"
-
-##setxkbmap -option caps:escape
-##sleep 5
+#setxkbmap -option "caps:swapescape"
+setxkbmap -option caps:escape
+sleep 5
 ##xcape -e 'Caps_Lock=Control_L|B'
 
