fcc79c52a8b17ae6ea7f339192200fd609a1c735
mort1skoda <mort1skoda@gmail.com>
2023-02-01 08:04:48 +0100

    [2023-02-01 07:46:35]

---
+++ target/path/scripts/save-snapshot.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "$CURRENT_DIR/helpers.sh"
+source "$CURRENT_DIR/variables.sh"
+
+main() {
+  local name="$1"
+  local resurrect_save_script_path="$(get_tmux_option "$resurrect_save_path_option" "")"
+  if [ -n "$resurrect_save_script_path" ] && [ -n "$name" ]; then
+    tmux display-message "Saving snapshot '$name'..."
+    local last_file="$(last_resurrect_file)"
+    local original_path="$(last_resurrect_path)"
+    local name_path="$(resurrect_dir)/$name"
+
+    "$resurrect_save_script_path" "quiet" >/dev/null 2>&1
+    local last_snapshot="$(last_resurrect_path)"
+
+    if files_differ "$name_path" "$last_snapshot"; then
+      ln -fs "$last_snapshot" "$name_path"
+    fi
+
+    if [ -n "$original_path" ]; then
+      ln -fs "$original_path" "$last_file"
+    fi
+
+    tmux display-message "Snapshot '$name' saved"
+  fi
+}
+
+main "$@"
